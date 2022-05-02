bc40f56ffb3f29a349826ac9045744869da0f6f9
berbraga <bernardo.sbraga@yahoo.com>
2022-05-02 15:40:17 -0300

    bernardo: iniciando o projeto

---
+++ pokedx2/src/App.js
@@ -0,0 +1,20 @@
+import react from 'react';
+
+// colocar os imports de libs
+// import axios from 'axios';
+import styled from 'styled-components';
+
+
+const MainConstainer = styled.div`
+  display:flex;
+`
+
+export default function App() {
+  return (
+    <MainConstainer>
+      to aqui
+    </MainConstainer>
+  );
+}
+
+
