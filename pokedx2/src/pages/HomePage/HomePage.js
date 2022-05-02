7f3a8f1ca9b294b31e6351a4a4ccaff3bc8e9b06
Andresa Alves <andresa_15ga@hotmail.com>
2022-05-02 17:09:12 -0300

    Commit dia 01

---
+++ pokedx2/src/pages/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function HomePage() {
+
+    return (
+        <div>
+            <HomePageContainer>
+                <h1>Pokémon</h1>
+                <SelectCategory>
+                    <option>Tipo De Pokémon</option>
+                </SelectCategory>
+                <CardsContainer>
+ 
+                </CardsContainer>
+            </HomePageContainer>
+        </div>
+
+    )
+}
+
+export default HomePage
