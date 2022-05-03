2ec291c8598970281b7855f0138ee1b2a0247ef8
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-05-03 07:17:54 -0300

    Parte gráfica básica finalizada

--- pokedx2/src/pages/DetailsPage/DetailsPage.js
+++ pokedx2/src/pages/DetailsPage/DetailsPage.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Header from "./Header/Header"
 // colocar os imports de libs
 import {useNavigate} from 'react-router-dom';
-// import styled from 'styled-components';
 // import axios from 'axios';
 
 // colocar as importacoes de todos os arquivos de codigos do projeto
-import { goBack } from '../../routes/coordinator';
-import {MainContainer, Button, InfoContainer, Img, Stats, Type, Moves } from '../../styles/styles';
-
+// import { goBack } from '../../routes/coordinator';
+// import {MainContainer, Button, InfoContainer, Img, Stats, Type, Moves } from '../../styles/styles';
+import {Container, ContainerImg, ContainerPowers, ContainerDetails, ContainerType, ContainerAttack} from "./styled"
 
 const DetailPage = (props) => {
     const navigate = useNavigate();
@@ -17,7 +16,6 @@ const DetailPage = (props) => {
     return(
         <>
 
-          <Header />
             {/* <Header>
 
                 <Button 
@@ -27,7 +25,7 @@ const DetailPage = (props) => {
                 <Button>BOTÃO ADICIONAR/REMOVER DA POKEDEX</Button>
 
             </Header> */}
-            <InfoContainer>
+            {/* <InfoContainer>
                 <InfoContainer>
                     <Img
                         src="http://3.bp.blogspot.com/_M82AcW40FFc/SwrH0Cci3cI/AAAAAAAAAEQ/guCMPwYawwo/s1600/mr+burns.jpg" 
@@ -42,8 +40,43 @@ const DetailPage = (props) => {
                 <Type> TIPO DO POKEMON </Type>
                 <Moves> MOVES DO POKEMON </Moves>
 
-            </InfoContainer>
+            </InfoContainer> */}
             
+          <Header />
+
+          <Container>
+            <ContainerImg>
+              <img src="http://3.bp.blogspot.com/_M82AcW40FFc/SwrH0Cci3cI/AAAAAAAAAEQ/guCMPwYawwo/s1600/mr+burns.jpg" alt="" />
+              <img src="http://3.bp.blogspot.com/_M82AcW40FFc/SwrH0Cci3cI/AAAAAAAAAEQ/guCMPwYawwo/s1600/mr+burns.jpg" alt="" />
+            </ContainerImg>
+
+            <ContainerPowers>
+              <h3>Poderes</h3>
+              <p>hp: 00</p>
+              <p>attack: 00</p>
+              <p>defense: 00</p>
+              <p>special-attack: 00</p>
+              <p>special-defense: 00</p>
+              <p>speed: 00</p>
+            </ContainerPowers>
+
+            <ContainerDetails>
+              <ContainerType>
+                <p>type1</p>
+                <p>type2</p>
+              </ContainerType>
+              
+              <ContainerAttack>
+                <h3>Principais ataques</h3>
+                <p>ataque 1</p>
+                <p>ataque 2</p>
+                <p>ataque 3</p>
+                <p>ataque 4</p>
+                <p>ataque 5</p>
+              </ContainerAttack>
+
+            </ContainerDetails>
+          </Container>
 
         </>
     )
