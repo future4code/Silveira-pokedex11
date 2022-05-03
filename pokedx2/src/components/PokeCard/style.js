8f241e6d7e6eebfd22819598291a5517664f885a
Andresa Alves <andresa_15ga@hotmail.com>
2022-05-03 20:52:49 -0300

    HomePage quase finalizada

--- pokedx2/src/components/PokeCard/style.js
+++ pokedx2/src/components/PokeCard/style.js
@@ -1,8 +1,39 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  border: dotted 1px black;
-  /* margin: 10px; */
+ width: 250px;
+    height: 300px;
+    background-color: #292929;
+    border-radius: 30px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: center;
+    padding: 15px;
+    margin-left: 2vw;
+    color: white;
+    font-size: 16px;
+    box-shadow: 5px 5px 10px 2px #1C3144;
+    text-transform: capitalize;
+    div{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    a{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 210px;
+    height: 38px;
+    cursor:pointer;
+    }
+    &:hover{
+        transition: all 800ms;
+        transform: scale(1.05);
+        position:relative;
+        box-shadow: 5px 5px 10px 2px #e3293b;
+    }
 `
 export const Img = styled.img`
   width: 100%;
@@ -19,4 +50,8 @@ export const Button = styled.button`
   align-items: center;
   justify-content: center;
   font-size: 12px;
+`
+
+export const PokemonNameCard = styled.button`
+  color: white;
 `
--- pokedx2/src/pages/HomePage/style.js
+++ pokedx2/src/pages/HomePage/style.js
@@ -1,5 +1,38 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HomeContainer = styled.div`
-  background-color: #f6e652 ;
-`
+  background-color: #f6e652;
+`;
+
+export const SelectCategory = styled.select`
+  margin-bottom: 40px;
+  margin-top: 40px;
+  margin-left: 38vw;
+  background-color: #737383;
+  border-radius: 19px;
+  border: 2px solid #41414a;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  font-size: 18px;
+  padding: 6px 15px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #8f313a;
+  &:hover {
+    background-color: #41414a;
+  }
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+`;
+
+export const ContainerGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto;
+    row-gap: 30px;
+    column-gap: 30px;
+`;
